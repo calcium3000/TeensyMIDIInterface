519dcd86e73d5bb1b75e163507e620b6e5724d2d
Calvin A. Cherry <calvincherry@gmail.com>
2018-05-05 13:24:01 -0600

    Added header information to software programs and added software license file

--- software/Test_MIDIin/Test_MIDIin.ino
+++ software/Test_MIDIin/Test_MIDIin.ino
@@ -1,5 +1,29 @@
 /*
+ * Test_MIDIin.ino
+ * 
+ * Copyright 2018  Calvin A. Cherry (calvincherry@gmail.com)
+ *                 calcium3000.wordpress.com
+ * 
+ * Adopted from PRJC's MIDI Library page
  * https://www.pjrc.com/teensy/td_libs_MIDI.html
+ * 
+ * For use with the Teensy MIDI Interface board
+ * https://github.com/calcium3000/TeensyMIDIInterface
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
  */
 
 #include <MIDI.h>
